66e11e26ed9e4204a12b92a968d63d534a7f2ddc
AlexandrinVictor <aleksandrinvictor@gmail.com>
2020-02-29 22:02:10 +0500

    fix: sampler

--- rl_experiments/common/sampler.py
+++ rl_experiments/common/sampler.py
@@ -7,25 +7,16 @@ class Sampler:
     def __init__(
         self,
         env: gym.Env,
-<<<<<<< HEAD
-=======
-        batch_size: int = 32,
->>>>>>> add: sampler
         n_steps: int = 3
     ):
 
         self.env = env
         self.current_state = env.reset()
-<<<<<<< HEAD
-=======
-        self.batch_size = batch_size
->>>>>>> add: sampler
         self.n_steps = n_steps
 
     def sample(self, agent):
         trajectories = []
 
-<<<<<<< HEAD
         states_t, actions, rewards, states_tp1 = [], [], [], []
 
         for j in range(self.n_steps):
@@ -48,30 +39,3 @@ class Sampler:
             'rewards': rewards,
             'states_tp1': states_tp1,
         }
-=======
-        for i in range(self.batch_size):
-            states_t, actions, rewards, states_tp1 = [], [], [], []
-
-            for j in range(self.n_steps):
-                action = agent.get_actions(np.array([self.current_state]))
-                next_state, reward, done, _ = self.env.step(action)
-
-                states_t.append(self.current_state)
-                actions.append(action)
-                rewards.append(reward)
-                states_tp1.append(next_state)
-
-                self.current_state = next_state
-                if done:
-                    self.current_state = self.env.reset()
-                    break
-
-            trajectories.append((
-                states_t,
-                actions,
-                rewards,
-                states_tp1
-            ))
-
-        return trajectories
->>>>>>> add: sampler
